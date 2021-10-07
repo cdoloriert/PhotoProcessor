a77e8abec46a9a39fd35931263206150dc972c1f
CD <cdoloriert@gmail.com>
2021-10-07 16:03:07 -0400

    Initial Implementation

---
+++ src/test/java/com/cd/postprocessor/PhotoprocessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cd.postprocessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoprocessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
